001b7389473927ba9834e376b2c1d2ef3997adbf
Iskyy <iskycruise01@gmail.com>
2023-02-13 21:05:07 +0700

    todo

--- TODO.js
+++ TODO.js
@@ -1,3 +1,14 @@
-// todo fixed the bug
+// TODO fixed the bug
 // ! @BUG at Booklist page
 // ? the admin can delete the book even it is on issue
+
+// TODO add one tap login in email
+// ? using jwt, create a routes and send the token in url as url parameter and the routes will verify the token
+
+// TODO Admin dashboard update
+// ? change the dashboard bar graph into something or find a thing to monitor the data
+
+// TODO optimize sweetalert
+// ? create a function for sweet alerts
+// * success: the custom swal will not affect by the hover effect to continue
+// * error: the custom swal will allow the user to hover and stop the timer to read the error
