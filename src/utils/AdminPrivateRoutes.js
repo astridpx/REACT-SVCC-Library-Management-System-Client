0b3e3dd5b3e4ec3fd81996b19ef33845ec57dd68
Iskyy <iskycruise01@gmail.com>
2023-05-06 10:38:49 +0700

    changed auth utils dependency from admin into Librarian

--- src/utils/AdminPrivateRoutes.js
+++ src/utils/AdminPrivateRoutes.js
@@ -4,7 +4,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const AdminPrivateRoutes = () => {
   const auth = localStorage.getItem("role");
 
-  return auth === "admin" ? <Outlet /> : <Navigate to="/" />;
+  return auth === "Librarian" ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default AdminPrivateRoutes;
